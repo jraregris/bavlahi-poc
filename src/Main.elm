cac31b58f89b68c9a696927fa102c792671a1dfe
oddmunds <oddmunds@iterate.no>
2019-02-03 19:27:21 +0100

    better debug init

--- src/Main.elm
+++ src/Main.elm
@@ -4,26 +4,26 @@ import Browser
 import Html exposing (Html)
 
 
-main : Program flags model msg
+main : Program () model msg
 main =
     Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
 
 view : model -> Html msg
 view =
-    Debug.todo
+    Debug.todo "Make a view"
 
 
 init : flags -> ( model, Cmd msg )
 init =
-    Debug.todo
+    Debug.todo "Make an init"
 
 
 update : msg -> model -> ( model, Cmd msg )
 update =
-    Debug.todo
+    Debug.todo "Updateeee"
 
 
 subscriptions : model -> Sub msg
 subscriptions =
-    Debug.todo
+    Debug.todo "sub dub"
